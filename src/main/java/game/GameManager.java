e97bfaed52c30cd91030d9bf3cfcdb9356fbdb91
= <=>
2023-05-01 21:29:27 +0900

    Replay Game by Manager

--- src/main/java/game/GameManager.java
+++ src/main/java/game/GameManager.java
@@ -1,8 +1,20 @@
 package game;
 
+import java.util.Scanner;
+
 public class GameManager {
 
     public void execute(Game game){
-        game.play();
+        do{
+            game.play();
+        }while(replay());
+        System.out.println("게임을 종료합니다...");
+    }
+
+    public boolean replay(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("다시 하시겠습니까?(Y/y)");
+        String res = sc.next();
+        return "Y".compareToIgnoreCase(res) == 0;
     }
 }
